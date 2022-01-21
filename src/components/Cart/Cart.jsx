943b965452de63b95f408757fd68b0d3842d9a07
Aryan Sharma <aryan.co19@nsut.ac.in>
2022-01-21 12:02:54 +0530

    Added Cart Functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal/Modal";
+import CartContext from "../Store/Cart-Context";
+import { useContext } from "react/cjs/react.development";
+import CartItem from "./CartItem";
+
 const Cart = (props) => {
-  const cartItems = [
-    { id: "c1", name: "Sushi", amount: "2", price: 12.99 },
-  ].map((item) => <li>{item.name}</li>);
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `₹${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.lenght > 0;
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+  const cartItems = cartCtx.items.map((item) => (
+    <CartItem
+      key={item.id}
+      name={item.name}
+      amount={item.amount}
+      id={item.id}
+      price={item.price}
+      onAdd={cartItemAddHandler.bind(null, item)}
+      onRemove={cartItemRemoveHandler.bind(null, item.id)}
+    />
+  ));
+
   return (
     <Modal onClickClose={props.onClickClose}>
       <ul className={classes["cart-items"]}>{cartItems}</ul>
       <div className={classes.total}>
-          <span>Total Amount</span>
-          <span>35.62</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-          <button className={classes['button--alt']} onClick={props.onClickClose}>Close</button>
-          <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClickClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
